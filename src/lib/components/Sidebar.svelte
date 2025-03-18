addae8b209d1fa0a00ef3aa1705c9fdf462a3fee
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2025-03-18 18:01:58 +0500

    started

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script></script>
+
+<nav class="bg-base-200 fixed lg:top-0 left-0 bottom-0 lg:h-full lg:w-30 w-full h-30">
+    <ul class="flex lg:flex-col lg:justify-start justify-between items-center h-full py-10 lg:px-0 px-10">
+        <li class="h-25 w-25 rounded-md flex justify-center items-center">
+            <a class="flex flex-col justify-center items-center gap-3" href="/">
+                <i class="fa-solid fa-house scale-140"></i>
+                <p>Home</p>
+            </a>
+        </li>
+
+        <li class="h-25 w-25 rounded-md flex justify-center items-center">
+            <a class="flex flex-col justify-center items-center gap-3" href="/subjects">
+                <i class="fa-solid fa-book scale-140"></i>
+                <p>Subjects</p>
+            </a>
+        </li>
+
+        <li class="h-25 w-25 rounded-md flex justify-center items-center">
+            <a class="flex flex-col justify-center items-center gap-3" href="/profile">
+                <i class="fa-solid fa-user scale-140"></i>
+                <p>Profile</p>
+            </a>
+        </li>
+    </ul>
+</nav>
