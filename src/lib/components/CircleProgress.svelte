bff30468eaf21b6f4fb11574ba1e92a0e1df97cf
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2025-03-18 18:14:52 +0500

    created radial progress

---
+++ src/lib/components/CircleProgress.svelte
@@ -0,0 +1,14 @@
+<script>
+    let props = $props();
+</script>
+
+<div class="radial-progress text-primary"
+     style={`--value:${props.value}; --size:20rem; --thickness: 2rem;`}
+     aria-valuenow="20"
+     role="progressbar"
+>
+    <div class="text-center text-base-content text-3xl">
+        <p class="text-accent text-4xl">{props.value}%</p>
+        <p>Completed</p>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import CircleProgress from "$lib/components/CircleProgress.svelte";
+</script>
+
+<div class="p-10">
+    <CircleProgress value="20"/>
+</div>
